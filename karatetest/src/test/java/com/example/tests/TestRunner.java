6fa431339f1263417c73cc7ccc11e190eee2acb7
rkouki-ext <rim.kouki-ext@be-ys.com>
2024-10-31 11:06:35 +0100

    Initial commit

---
+++ karatetest/src/test/java/com/example/tests/TestRunner.java
@@ -0,0 +1,22 @@
+package com.example.tests;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+public class TestRunner {
+
+    @Test
+    void runAllTests() {
+        Results results = Runner.path(
+                        "classpath:Accostage-Rating-CPM/IVSQA-TC-55.feature"
+
+                )
+                .outputCucumberJson(true)
+                .parallel(1);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+}
